b90c340f5a3c3d45196d8fc23b1bbf0f576e0826
George7836 <96310979+George7836@users.noreply.github.com>
2023-05-04 21:40:36 +0300

    fix: add a placemark on the map

--- src/pages/YandexMap.tsx
+++ src/pages/YandexMap.tsx
@@ -1,22 +1,23 @@
-import { useYMaps } from '@pbe/react-yandex-maps'
+import { Placemark, YMaps, useYMaps, Map } from '@pbe/react-yandex-maps'
 import React, { useEffect, useRef } from 'react'
 import { ArrowDownIcon, DeleteIcon, Preloader, UpdateIcon } from '../icons'
 import PageContainer from '../components/PageContainer'
+import ymaps from 'yandex-maps'
 
 export default function YandexMap() {
-  const mapRef = useRef(null)
+  // const mapRef = useRef(null)
   const ymaps = useYMaps(['Map'])
 
-  useEffect(() => {
-    if (!ymaps || !mapRef.current) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!ymaps || !mapRef.current) {
+  //     return;
+  //   }
 
-    new ymaps.Map(mapRef.current, {
-      center: [56.74160758449387,37.16145249999998],
-      zoom: 12,
-    });
-  }, [ymaps]);
+  //   new ymaps.Map(mapRef.current, {
+  //     center: [56.74160758449387,37.16145249999998],
+  //     zoom: 12,
+  //   });
+  // }, [ymaps]);
 
   return (
       <div className='map'>
@@ -35,7 +36,18 @@ export default function YandexMap() {
           </div>
         </div>
           {ymaps 
-            ? <div ref={mapRef} className='map__field' />
+            // ? <div ref={mapRef} className='map__field'></div>
+            ? <YMaps>
+                <Map
+                  className='map__field'
+                  defaultState={{
+                    center: [56.32143738580229,44.00381608593748],
+                    zoom: 10,
+                  }}
+                >
+                  <Placemark geometry={[56.32143738580229,44.00381608593748]} />
+                </Map>
+              </YMaps>
             : <div className='preloader'>
                 <Preloader/>
               </div>
